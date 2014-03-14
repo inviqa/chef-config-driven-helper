5de3604afaaeaf0b6e4c0ef9e47322bd6584f08e
Mike Simons <msimons@inviqa.com>
2014-03-14 13:58:45 +0100

    Install packages from node config

--- recipes/mysql-user.rb
+++
@@ -1,9 +0,0 @@
-if node["mysql"] && node["mysql"]["users"]
-  node["mysql"]["users"].each do |name, details|
-    mysql_database_user name do
-      details.each do |key, value|
-        self.send key, value
-      end
-    end
-  end
-end
