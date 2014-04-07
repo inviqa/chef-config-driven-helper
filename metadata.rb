3c4c1009bf3bce08078f522d83318a4031278431
Mike Simons <msimons@inviqa.com>
2014-04-07 10:58:45 +0200

    Version bump to 1.0.4

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Inviqa"
 maintainer_email  "msimons@inviqa.com"
 license           "Apache 2.0"
 description       "enable driving cookbooks that are not normally config driven to be so"
-version           "1.0.3"
+version           "1.0.4"
 
 depends "apache2"
 depends "database"
