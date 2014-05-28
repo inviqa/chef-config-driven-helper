e66e2cc2b3cb3be3130b1fdb38ac24fa51416ec4
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2014-05-28 14:37:25 +0100

    support angular apps

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['nginx-sites']['secure_port'] = 443
 default['nginx-sites']['insecure_port'] = 80
 default['nginx-sites']['endpoint'] = 'index.php'
+default['nginx-sites']['php_support'] = true
 default['nginx-sites']['php_fpm_port'] = 9000
 default['nginx-sites']['php_fpm_host'] = '127.0.0.1'
 default['nginx-sites']['ssl'] = false
