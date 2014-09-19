631764a70696bf5652d1b00aa63f077f717803bf
Mike Simons <msimons@inviqa.com>
2014-09-19 13:43:15 +0200

    Forcess mysql-ruby to install mysql gem in to chef-omnibus ruby

--- recipes/mysql-ruby.rb
+++ recipes/mysql-ruby.rb
@@ -2,6 +2,6 @@ include_recipe 'mysql::client'
 include_recipe 'build-essential'
 
 gem_package "mysql" do
-  gem_binary nil
+  gem_binary File.join(RbConfig::CONFIG["bindir"], 'gem')
   action :install
 end
