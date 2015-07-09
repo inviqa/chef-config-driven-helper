bf58619c4d23faf6601b50bf5f88d64828f61959
Kieren Evans <kevans@inviqa.com>
2015-07-09 15:27:02 +0100

    Fix double quotes vs single quotes.

--- test/.Vagrantfile.disable-berkshelf.rb
+++ test/.Vagrantfile.disable-berkshelf.rb
@@ -1,5 +1,5 @@
 Vagrant.configure('2') do |config|
-  if Vagrant.has_plugin?("vagrant-berkshelf") then
+  if Vagrant.has_plugin?('vagrant-berkshelf') then
     config.berkshelf.enabled = false
   end
 end
