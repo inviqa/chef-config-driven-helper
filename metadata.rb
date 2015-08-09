9ec871bf917c536ffe2ca969ec4c584396f54cb3
Andy Thompson <me@andytson.com>
2015-08-09 10:19:00 +0100

    Bump version to 1.4.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Inviqa"
 maintainer_email  "msimons@inviqa.com"
 license           "Apache 2.0"
 description       "enable driving cookbooks that are not normally config driven to be so"
-version           "1.4.0"
+version           "1.4.1"
 
 depends "apache2", "~> 1.8"
 depends 'iptables-ng', '~> 2.2.0'
