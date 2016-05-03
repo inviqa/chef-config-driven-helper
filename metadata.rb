f33fb74ec4e57b9fe5aa854ff565971094dd1c0a
Andy Thompson <athompson@inviqa.com>
2016-05-03 17:24:52 +0100

    Create separate test Berksfiles for testing old and loose constraints

--- metadata.rb
+++ metadata.rb
@@ -5,15 +5,15 @@ issues_url        "https://github.com/inviqa/chef-config-driven-helper/issues"
 source_url        "https://github.com/inviqa/chef-config-driven-helper"
 license           "Apache 2.0"
 description       "enable driving cookbooks that are not normally config driven to be so"
-version           "1.5.2"
+version           "1.6.0"
 
-depends "apache2", "~> 1.8"
-depends 'iptables-ng', '~> 2.2.0'
+depends "apache2", ">= 1.8"
+depends 'iptables-ng', '>= 2.2'
 depends "mysql", "~> 4.0"
 depends 'nginx', '< 2.4.4'
-depends "database", "~> 2.0"
-depends "build-essential", "~> 1.4"
-depends 'ssh_known_hosts', '~> 2.0'
+depends "database", ">= 2.0"
+depends "build-essential", ">= 1.4"
+depends 'ssh_known_hosts', '>= 2.0'
 
 %w{ debian ubuntu centos redhat fedora scientific amazon windows }.each do |os|
   supports os
