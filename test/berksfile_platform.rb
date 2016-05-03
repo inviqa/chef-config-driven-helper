6427c1aa74c67d399cf282cd54c500851409485c
Andy Thompson <athompson@inviqa.com>
2016-05-03 14:16:47 +0100

    Change test Berksfile to use metadata DSL to work correctly with metadata dependencies
    This stops Berksfile picking an old cookbook versions.
    A monkey-patch was needed to make berkshelf then run in the test/ folder in Test-Kitchen

--- test/berksfile_platform.rb
+++ test/berksfile_platform.rb
@@ -1,5 +1,6 @@
 require 'kitchen/provisioner/chef_base'
-require 'kitchen/provisioner/chef/common_sandbox.rb'
+require 'kitchen/provisioner/chef/common_sandbox'
+require 'kitchen/provisioner/chef/berkshelf'
 
 Kitchen::Provisioner::ChefBase.class_eval do
   default_config :berksfile, 'test/Berksfile'
@@ -16,5 +17,14 @@ Kitchen::Provisioner::Chef::CommonSandbox.class_eval do
 
   def berksfile
     File.join(config[:kitchen_root], config[:berksfile])
+  end
+end
+
+Kitchen::Provisioner::Chef::Berkshelf.class_eval do
+  alias :_resolve :resolve
+  def resolve
+    Dir.chdir(File.dirname(@berksfile)) do
+      _resolve
+    end
   end
 end
