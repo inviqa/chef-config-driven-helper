ba807b71e3eaa0f71fa22f4c9d7df0f50c62a09c
Andy Thompson <athompson@inviqa.com>
2016-05-04 12:37:35 +0100

    Bump major version release
    Major version bump is because the constraint changes will affect projects that don't have adequate pinning of config-driven-helper's dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name              "config-driven-helper"
 maintainer        "Inviqa"
-maintainer_email  "msimons@inviqa.com"
+maintainer_email  "athompson@inviqa.com"
 issues_url        "https://github.com/inviqa/chef-config-driven-helper/issues"
 source_url        "https://github.com/inviqa/chef-config-driven-helper"
 license           "Apache 2.0"
 description       "enable driving cookbooks that are not normally config driven to be so"
-version           "1.6.0"
+version           "2.0.0"
 
 depends "apache2", ">= 1.8"
 depends 'iptables-ng', '>= 2.2'
