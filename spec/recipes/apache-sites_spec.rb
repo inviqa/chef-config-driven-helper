dcb14bc639285bb2314448e58fe617f0c8bcac91
Kieren Evans <kevans@inviqa.com>
2016-06-21 11:42:43 +0100

    Fix spec to write out /etc/apache2/sites-available/hello.example.com

--- spec/recipes/apache-sites_spec.rb
+++ spec/recipes/apache-sites_spec.rb
@@ -9,9 +9,13 @@ describe 'config-driven-helper::apache-sites' do
     cached(:chef_run) do
       ChefSpec::SoloRunner.new do |node|
         node.set['apache']['version'] = '2.2'
-        node.set['apache']['sites']['hello']['docroot'] = '/var/www/hello.example.com'
-        node.set['apache']['sites']['hello']['server_name'] = 'hello.example.com'
+        node.set['apache']['sites']['hello.example.com']['docroot'] = '/var/www/hello.example.com'
+        node.set['apache']['sites']['hello.example.com']['server_name'] = 'hello.example.com'
       end.converge(described_recipe)
+    end
+
+    it 'will write out a hello.example.com vhost' do
+      expect(chef_run).to create_template('/etc/apache2/sites-available/hello.example.com')
     end
 
     it 'will write out apache 2.2 configuration when 2.2 is in use' do
@@ -27,9 +31,13 @@ describe 'config-driven-helper::apache-sites' do
     cached(:chef_run) do
       ChefSpec::SoloRunner.new do |node|
         node.set['apache']['version'] = '2.4'
-        node.set['apache']['sites']['hello']['docroot'] = '/var/www/hello.example.com'
-        node.set['apache']['sites']['hello']['server_name'] = 'hello.example.com'
+        node.set['apache']['sites']['hello.example.com']['docroot'] = '/var/www/hello.example.com'
+        node.set['apache']['sites']['hello.example.com']['server_name'] = 'hello.example.com'
       end.converge(described_recipe)
+    end
+
+    it 'will write out a hello.example.com vhost' do
+      expect(chef_run).to create_template('/etc/apache2/sites-available/hello.example.com')
     end
 
     it 'will write out apache 2.4 configuration when 2.4 is in use' do
