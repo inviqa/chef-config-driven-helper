ace56c9d38341017a5f1f432e36fe43f9a8e3e0f
Kieren Evans <kevans@inviqa.com>
2016-06-21 16:13:16 +0100

    Fix specs when under apache 2.2 or 2.4

--- spec/recipes/apache-sites_spec.rb
+++ spec/recipes/apache-sites_spec.rb
@@ -19,11 +19,13 @@ describe 'config-driven-helper::apache-sites' do
     end
 
     it 'will write out apache 2.2 configuration when 2.2 is in use' do
-      expect(chef_run).to render_file('/etc/apache2/sites-available/hello.example.com').with_content('Order allow,deny')
+      expect(chef_run).to render_file('/etc/apache2/sites-available/hello.example.com')
+        .with_content('Order allow,deny')
     end
 
     it 'will not write out apache 2.4 configuration when 2.2 is in use' do
-      expect(chef_run).to_not render_file('/etc/apache2/sites-available/hello.example.com').with_content('Require all granted')
+      expect(chef_run).to_not render_file('/etc/apache2/sites-available/hello.example.com')
+        .with_content('Require all granted')
     end
   end
 
@@ -37,15 +39,16 @@ describe 'config-driven-helper::apache-sites' do
     end
 
     it 'will write out a hello.example.com vhost' do
-      expect(chef_run).to create_template('/etc/apache2/sites-available/hello.example.com')
+      expect(chef_run).to create_template('/etc/apache2/sites-available/hello.example.com.conf')
     end
 
     it 'will write out apache 2.4 configuration when 2.4 is in use' do
-      expect(chef_run).to render_file('/etc/apache2/sites-available/hello.example.com').with_content('Require all granted')
+      expect(chef_run).to render_file('/etc/apache2/sites-available/hello.example.com.conf')
+        .with_content('Require all granted')
     end
 
     it 'will not write out apache 2.2 configuration when 2.4 is in use' do
-      expect(chef_run).to_not render_file('/etc/apache2/sites-available/hello.example.com').with_content('Order allow,deny')
+      expect(chef_run).to_not render_file('/etc/apache2/sites-available/hello.example.com.conf').with_content('Order allow,deny')
     end
   end
 end
