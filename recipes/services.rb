bbefb89332d0af9c1c2622b1a2777db0024bf28e
Kieren Evans <kevans@inviqa.com>
2016-06-22 18:22:34 +0100

    Force service control block to trigger.
    Say an existing service resource "service 'test' do action :enable end" and the desire is
    to get the service to enable and start via config-driven-helper::services.
    
    If chef runs and `"services": {"test": ["enable","start"]}` is defined as configuration,
    and the service is already enabled but not started, chef considers the service resource
    as having nothing to do, so the previous iteration of this recipe would simply do nothing.
    
    This was due to the ruby block only being triggered by the service having been updated
    previously in the run.

--- recipes/services.rb
+++ recipes/services.rb
@@ -3,6 +3,7 @@ if node['services']
     service k do
       action :nothing
     end
+
     ruby_block "service control (#{k})" do
       block do
         r = resources(:service => k)
@@ -12,6 +13,12 @@ if node['services']
       end
       subscribes :run, "service[#{k}]", :delayed
       action :nothing
+    end
+
+    log "service control (#{k})" do
+      message "config-driven-helper::services queueing actions [#{v.flatten.join(', ')}] for service #{k}"
+      level :debug
+      notifies :run, "ruby_block[service control (#{k})]", :delayed
     end
   end
 end
