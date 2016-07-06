bbacd8f8645fa2493eacffd497b062f94f09d602
Felicity Ratcliffe <felicityratcliffe@Felicitys-MacBook-Pro-2.local>
2016-07-06 11:04:38 +0100

    add raise exception to old recipe

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,3 +1,7 @@
+if Gem::Dependency.new('chef', Chef::Version).match('chef', '>= 12.10')
+  raise 'config-driven-helper::packages not supported in chef >= 12.10. Please use config-driven-helper::packages-additional instead, which uses a different syntax'
+end
+
 if node['packages-additional']
   node['packages-additional'].each do |name, data|
     data = { action: data } if data.is_a?(String)
