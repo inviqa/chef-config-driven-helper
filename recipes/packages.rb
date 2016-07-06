bccabc645ca73a08538ebd70c1c400769a88c08b
Felicity Ratcliffe <felicityratcliffe@Felicitys-MacBook-Pro-2.local>
2016-07-06 11:57:51 +0100

    revert old recipe to original syntax

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -2,13 +2,10 @@ if Gem::Dependency.new('chef', Chef::Version).match('chef', '>= 12.10')
   raise 'config-driven-helper::packages not supported in chef >= 12.10. Please use config-driven-helper::packages-additional instead, which uses a different syntax'
 end
 
-if node['packages-additional']
-  node['packages-additional'].each do |name, data|
-    data = { action: data } if data.is_a?(String)
+if node['packages']
+  node['packages'].each do |name|
     package name do
-      data.each do |key,value|
-        self.send key, value
-      end
+      action :install
     end
   end
 end
