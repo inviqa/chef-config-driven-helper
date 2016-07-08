3eb351a4b4ae2acba8c88907dce94d47c6d3e618
Andy Thompson <athompson@inviqa.com>
2016-07-08 21:07:48 +0100

    Fix issue with apache2 cookbook < 2 apache_site enable
    It's not that the httpd software version is 2.4 causing the .conf addition but the apache2 cookbook version
    
    This issue was causing httpd 2.2 to not link sites-available to sites-enabled
    
    Tests have been added to ensure the "Hello world" is rendered, which catches this and other issues like mod_php5 not being enabled

--- test/cookbooks/config-driven-helper-test/recipes/apache-include.rb
+++ test/cookbooks/config-driven-helper-test/recipes/apache-include.rb
@@ -1,3 +1,5 @@
-file "#{node['apache']['conf_dir']}/test.conf" do
+conf_dir = node['apache']['conf_dir'] || "#{node['apache']['dir']}/conf"
+
+file "#{conf_dir}/test.conf" do
   content "# Test include file"
 end
